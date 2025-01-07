4090b88af75ef0fff08c9ee9239320085b242841
s0nt3k <53980972+s0nt3k@users.noreply.github.com>
2025-01-07 12:32:38 -0800

    Create Install-DnsBlockingScript.cmd

---
+++ Bash/Install-DnsBlockingScript.cmd
@@ -0,0 +1,5 @@
+Echo Download & Execute PS1 Script
+
+Invoke-WebRequest -Uri "https://github.com/s0nt3k/PowerShell/blob/main/Scripts/HostsDnsBlocklist.ps1" -OutFile .\HostsDnsBlocklist.ps1
+
+Start-Process powershell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File .\HostsDnsBlocklist.ps1' -Verb RunAs
